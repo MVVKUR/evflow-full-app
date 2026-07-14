d7beb85c5b67185a2bb7e8bad96d40b0ee95a07c
Nicholas <nichz123@gmail.com>
2026-07-14 20:32:55 +0700

    Add information related to test data credentials TODO: change the username, email, and password to reflect real data

--- frontend-evflow-app/packages/ui/src/styles/styles.ts
+++ frontend-evflow-app/packages/ui/src/styles/styles.ts
@@ -298,6 +298,13 @@ export const loginScreenStyles = StyleSheet.create({
     color: colors.text,
     fontWeight: '700'
   },
+  markerCredentialsInfo: {
+    color: '#1f2529',
+    fontSize: fontSizes.control,
+    lineHeight: 20,
+    marginTop: 24,
+    textAlign: 'justify'
+  },
   content: {
     alignItems: 'center',
     maxWidth: layout.authPageMaxWidth,
